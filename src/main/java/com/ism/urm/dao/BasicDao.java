af0b182add6e9fe8f9509b3ecca745aae822c35d
jsmin <jsmin@jsminPC>
2020-03-12 17:52:51 +0900

    conflict 해결

--- src/main/java/com/ism/urm/dao/BasicDao.java
+++ src/main/java/com/ism/urm/dao/BasicDao.java
@@ -1,7 +1,6 @@
 package com.ism.urm.dao;
 
 import java.lang.reflect.ParameterizedType;
-import java.sql.SQLException;
 import java.util.List;
 
 import org.hibernate.Criteria;
@@ -12,53 +11,54 @@ import com.ism.urm.vo.manage.User;
 
 public abstract class BasicDao<T> {
 
-	protected Class<T> entityClass;
-	protected String entityName;
+    protected Class<T> entityClass;
+    protected String entityName;
 
 
-	public BasicDao() {
-		ParameterizedType type = (ParameterizedType) getClass().getGenericSuperclass();
-		entityClass = (Class<T>) type.getActualTypeArguments()[0];
-	}
+    public BasicDao() {
+        ParameterizedType type = (ParameterizedType) getClass().getGenericSuperclass();
+        entityClass = (Class<T>) type.getActualTypeArguments()[0];
+    }
 
-	public void save(Session session, T vo) throws SQLException {
-		session.saveOrUpdate(entityName, vo);
-	}
+    public void save(Session session, T vo) throws Exception {
+        session.saveOrUpdate(entityName, vo);
+    }
 
-	public T getById(Session session, String id) throws SQLException {//add
-		T rtn = (T) session.get(entityName, id);
-		return rtn;
-	}
+    public T getById(Session session, String id) throws Exception {
+        T rtn = (T) session.get(entityName, id);
+        return rtn;
+    }
 
-	public User getUserById(Session session, String id) throws SQLException {
-		User rtn = (User) session.get("USER", id);
-		return rtn;
-	}
+    public User getUserById(Session session, String id) throws Exception {
+        User rtn = (User) session.get("USER", id);
+        return rtn;
+    }
 
-	public List<T> list(Session session) throws SQLException {
-		return search(session, null);
-	}
+    public List<T> list(Session session) throws Exception {
+        return search(session, null);
+    }
 
-	public List<T> search(Session session, List<RelationOp> filter) throws SQLException {
-		Criteria crit = session.createCriteria(entityClass);
+    public List<T> search(Session session, List<RelationOp> filter) throws Exception {
+        Criteria crit = session.createCriteria(entityClass);
 
-		if (filter != null) {
-			for (RelationOp op : filter) {
-				crit.add(op.getCriterion());
-			}
-		}
+        if (filter != null) {
+            for (RelationOp op : filter) {
+                crit.add(op.getCriterion());
+            }
+        }
 
-		return crit.list();
-	}  
+        return crit.list();
+    }  
 
-	public abstract T get(Session session, String id) throws SQLException;
+    public abstract T get(Session session, String id) throws Exception;
+    public abstract void delete(Session session, String id) throws Exception;
 
-	public Class<T> getEntityClass() {
-		return entityClass;
-	}
+    public Class<T> getEntityClass() {
+        return entityClass;
+    }
 
-	public String getEntityName() {
-		return entityName;
-	}
+    public String getEntityName() {
+        return entityName;
+    }
 
 }
--- src/main/java/com/ism/urm/dao/manage/UserDao.java
+++ src/main/java/com/ism/urm/dao/manage/UserDao.java
@@ -1,40 +1,44 @@
 package com.ism.urm.dao.manage;
 
-import java.sql.SQLException;
 import org.hibernate.Session;
 import com.ism.urm.dao.BasicDao;
 import com.ism.urm.vo.manage.User;
 
 public class UserDao extends BasicDao<User> {
 
-	public UserDao() {
-	}
+    public UserDao() {
+    }
 
-	public User get(Session session, String id) throws SQLException {
-		User rtn = getUserById(session, id);
-		return rtn;
-	}
+    public User get(Session session, String id) throws Exception {
+        User rtn = getUserById(session, id);
+        return rtn;
+    }
+
+    @Override
+    public void delete(Session session, String id) throws Exception {
+        // TODO Auto-generated method stub
+    }
 }  
 
 //public class UserDao {
-//	String entityName;
-//	public UserDao() {
-//	}
+//    String entityName;
+//    public UserDao() {
+//    }
 //
-//	public User get(Session session, String id) throws SQLException {
-//		User rtn = getById(session, id);
-//		System.out.println("rtn:" + rtn);
-//		return rtn;
-//	}
+//    public User get(Session session, String id) throws SQLException {
+//        User rtn = getById(session, id);
+//        System.out.println("rtn:" + rtn);
+//        return rtn;
+//    }
 //
-//	public void setChild(Session session, User vo) throws SQLException {
+//    public void setChild(Session session, User vo) throws SQLException {
 //
-//	}
-//	
-//	public User getById(Session session, String id) throws SQLException{
-//		User rtn = (User) session.get(entityName, id);
-//		return rtn;
-//	}
+//    }
+//    
+//    public User getById(Session session, String id) throws SQLException{
+//        User rtn = (User) session.get(entityName, id);
+//        return rtn;
+//    }
 //
 //}
 
