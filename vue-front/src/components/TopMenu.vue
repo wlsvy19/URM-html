14bcc6dead5a3c2fb3b85a595ac6efd0576e5b11
Lee Jin Pyo <leejp@solulink.co.kr>
2021-02-24 10:32:26 +0900

    topmenu

--- vue-front/src/components/TopMenu.vue
+++ vue-front/src/components/TopMenu.vue
@@ -10,6 +10,15 @@
         <el-menu-item index="user">{{$t('urm.user')}}</el-menu-item>
         <el-menu-item index="biz">{{$t('urm.biz')}}</el-menu-item>
       </el-submenu>
+      <el-submenu index="Stat">
+        <template slot="title">{{$t('urm.stat')}}</template>
+        <el-menu-item-group>
+          <span slot="title">요건진행상태</span>
+          <el-menu-item>일별 진행상태</el-menu-item>
+          <el-menu-item>월별 진행상태</el-menu-item>
+          
+        </el-menu-item-group>
+      </el-submenu>
     </el-menu>
   </div>
 </template>
