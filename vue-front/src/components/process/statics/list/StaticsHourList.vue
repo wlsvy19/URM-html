494f09716ef1c359ad59e37114484415e31524c8
Lee Jin Pyo <leejp@solulink.co.kr>
2021-03-10 16:50:34 +0900

    거래처리통계 컬럼 너비 조정

--- vue-front/src/components/process/statics/list/StaticsHourList.vue
+++ vue-front/src/components/process/statics/list/StaticsHourList.vue
@@ -3,31 +3,31 @@
     <el-table :data="items" :height="listHeight" border stripe>
       <el-table-column label="처리날짜" prop="processDate" min-width="90"/>
       <el-table-column label="인터페이스ID" prop="interfaceId" width="150"/>
-      <el-table-column label="항목" prop="countType" min-width="115"/>
-      <el-table-column label="00" prop="hour00"/>
-      <el-table-column label="01" prop="hour01"/>
-      <el-table-column label="02" prop="hour02"/>
-      <el-table-column label="03" prop="hour03"/>
-      <el-table-column label="04" prop="hour04"/>
-      <el-table-column label="05" prop="hour05"/>
-      <el-table-column label="06" prop="hour06"/>
-      <el-table-column label="07" prop="hour07"/>
-      <el-table-column label="08" prop="hour08"/>
-      <el-table-column label="09" prop="hour09"/>
-      <el-table-column label="10" prop="hour10"/>
-      <el-table-column label="11" prop="hour11"/>
-      <el-table-column label="12" prop="hour12"/>
-      <el-table-column label="13" prop="hour13"/>
-      <el-table-column label="14" prop="hour14"/>
-      <el-table-column label="15" prop="hour15"/>
-      <el-table-column label="16" prop="hour16"/>
-      <el-table-column label="17" prop="hour17"/>
-      <el-table-column label="18" prop="hour18"/>
-      <el-table-column label="19" prop="hour19"/>
-      <el-table-column label="20" prop="hour20"/>
-      <el-table-column label="21" prop="hour21"/>
-      <el-table-column label="22" prop="hour22"/>
-      <el-table-column label="23" prop="hour23"/>
+      <el-table-column label="항목" prop="countType" min-width="130"/>
+      <el-table-column label="00" prop="hour00" width="70"/>
+      <el-table-column label="01" prop="hour01" width="70"/>
+      <el-table-column label="02" prop="hour02" width="70"/>
+      <el-table-column label="03" prop="hour03" width="70"/>
+      <el-table-column label="04" prop="hour04" width="70"/>
+      <el-table-column label="05" prop="hour05" width="70"/>
+      <el-table-column label="06" prop="hour06" width="70"/>
+      <el-table-column label="07" prop="hour07" width="70"/>
+      <el-table-column label="08" prop="hour08" width="70"/>
+      <el-table-column label="09" prop="hour09" width="70"/>
+      <el-table-column label="10" prop="hour10" width="70"/>
+      <el-table-column label="11" prop="hour11" width="70"/>
+      <el-table-column label="12" prop="hour12" width="70"/>
+      <el-table-column label="13" prop="hour13" width="70"/>
+      <el-table-column label="14" prop="hour14" width="70"/>
+      <el-table-column label="15" prop="hour15" width="70"/>
+      <el-table-column label="16" prop="hour16" width="70"/>
+      <el-table-column label="17" prop="hour17" width="70"/>
+      <el-table-column label="18" prop="hour18" width="70"/>
+      <el-table-column label="19" prop="hour19" width="70"/>
+      <el-table-column label="20" prop="hour20" width="70"/>
+      <el-table-column label="21" prop="hour21" width="70"/>
+      <el-table-column label="22" prop="hour22" width="70"/>
+      <el-table-column label="23" prop="hour23" width="70"/>
     </el-table>
   </div>
 </template>
