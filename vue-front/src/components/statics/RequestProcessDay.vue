236803707c80b2fe97daa5fa51bca19aa4a8f5d8
ok <ok@DESKTOP-1B3SF04>
2021-03-12 08:35:11 +0900

    .

--- vue-front/src/components/statics/RequestProcessDay.vue
+++ vue-front/src/components/statics/RequestProcessDay.vue
@@ -9,7 +9,7 @@
           </el-select>
         </el-form-item>
         <el-form-item label="일자 선택">
-          <el-date-picker v-model="dateRange" type="daterange" value-format="yyyyMMdd" class="no-suffix" :clearable="false"/>
+          <el-date-picker v-model="dateRange" type="daterange" value-format="yyyyMMdd" class="no-suffix" :clearable="false" :picker-options="pickerOptions"/>
         </el-form-item>
       </el-form>
       <div class="search-buttons">
@@ -33,6 +33,11 @@ export default {
         type: '',
       },
       pageUrl: '/process/day',
+      pickerOptions: {
+        disabledDate(time) {
+          return time.getTime() > Date.now()
+        }
+      }
     }
   },
   mounted () {
